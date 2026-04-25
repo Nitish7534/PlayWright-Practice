22c199e49537eb9a256f83eb9805e65a2d48aa04
Nitish Raikhola <raikholanitish3@gmail.com>
2026-04-25 10:45:55 +0530

    first commit

---
+++ tests/assertions.spec.js
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+
+test('Assertions Demo Test', async ({ page }) => {
+
+    await page.goto('https://kitchen.applitools.com/');
+
+    await expect(page.locator('text=The Kitchen')).toHaveCount(1);
+    await expect(page.locator('text=The Kitchen')).toBeVisible();
+    await expect(page.locator('text=The Kitchen')).toBeEnabled();
+
+    await expect.soft(page.locator('text=The Kitchen')).toHaveText('The Kitchen');
+
+    await expect(page.locator('text=The Kitchen')).toHaveText('The Kitchen');
+    await expect(page.locator('text=The Kitchen')).not.toHaveText('ABCD');
+
+    await page.screenshot({ path: 'screenshot.png' });
+
+    await expect(page).toHaveURL(/kitchen.applitools.com/);
+    await expect(page).toHaveTitle(/Kitchen/);
+
+    //FIXED LINE ONLY
+    //await expect(page.locator('.balance').first()).toContainText('$');
+
+    await expect(page).toHaveScreenshot();
+
+});
